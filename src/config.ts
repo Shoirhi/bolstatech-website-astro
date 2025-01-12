cbfa718c331eab16966520d15ee1c9a124beee90
Shoirhi <men_09_ahem@icloud.com>
2025-01-12 14:58:26 +0900

    Remove robots.txt API route and enhance site configuration with social media links and source code reference
    - Deleted the robots.txt API route implementation from `public/robots.txt.ts`.
    - Updated `src/config.ts` to include social media links for Twitter and GitHub, as well as a reference to the source code repository.
    - Modified `src/types.ts` to reflect the new structure of the `SiteConfig` interface, adding fields for social media and source code.
    
    These changes streamline the project by removing unnecessary code and improving the configuration for better visibility of social media presence and source code access.

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,9 @@ export const SITE: SiteConfig = {
   title: "BOLSTA TECH",
   description:
     "個人が運営するブログサイトです。Web開発、サイバーセキュリティの技術ブログや制作物を公開しています。たまにゲームや趣味の記事も投稿しています。",
+  sns: {
+    twitter: "https://x.com/bolstatech",
+    github: "https://github.com/shoirhi",
+  },
+  sourceCode: "https://github.com/shoirhi/bolstatech-website-astro",
 };
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,9 @@
 export interface SiteConfig {
   title: string;
   description: string;
+  sns: {
+    twitter: string;
+    github: string;
+  };
+  sourceCode: string;
 }
